3650cd1b691e3e6cad7898d6980b34a337b70fce
James Choa <triggerhappyelite@gmail.com>
2020-02-25 16:12:23 +0800

    Refactor clang-tidy and clang-format targets
    Creates a meta target that can be used to run dependent targets.
    Said dependent targets are the ones that actually run the
    clang-tidy and clang-format commands.

--- cmake/clang_tools.cmake
+++ cmake/clang_tools.cmake
@@ -3,24 +3,20 @@
 
 find_program(CLANG_TIDY NAMES clang-tidy clang-tidy-6.0)
 if (CLANG_TIDY)
-    add_custom_target(
-            clang-tidy
-            COMMAND ${CLANG_TIDY}
-            ${SOURCE_FILES}
-            --
-            -std=c++11
-            -I ${CMAKE_SOURCE_DIR}/include
-    )
+  add_custom_target(
+    all-tidy
+    COMMAND echo
+    "Running clang-tidy against all targets"
+  )
 endif ()
 
 find_program(CLANG_FORMAT NAMES clang-format clang-format-6.0)
 if (CLANG_FORMAT)
-    add_custom_target(
-            clang-format
-            COMMAND ${CLANG_FORMAT}
-            -i
-            ${SOURCE_FILES} ${TEST_SOURCE_FILES}
-    )
+  add_custom_target(
+    all-format
+    COMMAND echo
+    "Running clang-format against all built sources"
+  )
 endif ()
 
 option(ADDRESS_SANITIZER "Enable Clang AddressSanitizer" OFF)
