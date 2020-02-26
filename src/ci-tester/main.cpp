7ef635895fd13426fc8045d8ec185e17ad8bac52
James Choa <triggerhappyelite@gmail.com>
2020-02-26 22:42:08 +0800

    feat: add doNothing function

--- src/ci-tester/main.cpp
+++ src/ci-tester/main.cpp
@@ -4,8 +4,13 @@ int doSomething() {
   return 42;
 }
 
+void doNothing() {
+  std::cout << "This function doesn't really do anyting" << std::endl;
+}
+
 auto main(int /*argc*/, char * * /*argv*/) -> int {
   std::cout << "Testing" << std::endl;
   std::cout << doSomething() << std::endl;
+  doNothing();
   return 0;
 }
