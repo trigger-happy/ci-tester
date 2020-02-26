96c1624b967fb345d3b79de12a53c78bda891910
James Choa <triggerhappyelite@gmail.com>
2020-02-26 22:40:47 +0800

    feat: add doSomething function

--- src/ci-tester/main.cpp
+++ src/ci-tester/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int doSomething() {
+  return 42;
+}
+
 auto main(int /*argc*/, char * * /*argv*/) -> int {
   std::cout << "Testing" << std::endl;
+  std::cout << doSomething() << std::endl;
   return 0;
 }
